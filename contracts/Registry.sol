a039c9f4270d9df3689583d8dcbd93ade1fbc59c
didil <haritahadil@yahoo.fr>
2017-10-26 15:44:24 +0100

    owner can set address

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,24 +1,30 @@
 pragma solidity ^0.4.15;
 
 
-contract Registry {
+import "./Ownable.sol";
+
+
+contract Registry is Ownable {
     /*
      *  Events
      */
     event AddressAdded(address indexed userAddress);
+
+    event StateUpdated(address indexed userAddress, State state);
 
     /*
      *  Storage
      */
 
     mapping (address => UserData) public userDataStore;
+
     address[] public userAddresses;
 
-    enum State {NEW, VERIFIED, LOCKED}
+    enum State {NEW, VERIFIED, EXPIRED}
 
     struct UserData {
-      State state;
-      bool isUserData;
+    State state;
+    bool isUserData;
     }
 
     /*
@@ -30,7 +36,7 @@ contract Registry {
 
     /// @dev Allows to add an address to the registry
     /// @param _userAddress user address to be added
-    function addUserAddress(address _userAddress) public{
+    function addUserAddress(address _userAddress) public {
         // check user address not null
         require(_userAddress != address(0));
         // avoid overwrite
@@ -47,8 +53,23 @@ contract Registry {
 
     /// @dev Checks if a user address exists
     /// @param _userAddress user address to be checked
-    function isUserData(address _userAddress) public constant returns(bool) {
+    function isUserData(address _userAddress) public constant returns (bool) {
         return userDataStore[_userAddress].isUserData;
+    }
+
+    /// @dev Set state on user address
+    /// @param _userAddress user address
+    /// @param _state new state
+    function setState(address _userAddress, uint _state) public onlyOwner {
+        // check new state is valid
+        require(_state <= uint(State.EXPIRED));
+        // make sure user address exists
+        require(isUserData(_userAddress));
+
+        userDataStore[_userAddress].state = State(_state);
+
+        // Log event
+        StateUpdated(_userAddress, State(_state));
     }
 
 }
