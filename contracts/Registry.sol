9dcce5ad9c99b35675c09d36dd66b319903675bd
didil <haritahadil@yahoo.fr>
2017-10-26 11:42:37 +0100

    add user address

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -2,12 +2,53 @@ pragma solidity ^0.4.15;
 
 
 contract Registry {
+    /*
+     *  Events
+     */
+    event AddressAdded(address indexed userAddress);
+
+    /*
+     *  Storage
+     */
+
+    mapping (address => UserData) public userDataStore;
+    address[] public userAddresses;
+
+    enum State {NEW, VERIFIED, LOCKED}
+
+    struct UserData {
+      State state;
+      bool isUserData;
+    }
 
     /*
      * Public functions
      */
     /// @dev Contract constructor
     function Registry() {
+    }
+
+    /// @dev Allows to add an address to the registry
+    /// @param _userAddress user address to be added
+    function addUserAddress(address _userAddress) public{
+        // check user address not null
+        require(_userAddress != address(0));
+        // avoid overwrite
+        require(!isUserData(_userAddress));
+
+        // create new entry
+        userDataStore[_userAddress].state = State.NEW;
+        userDataStore[_userAddress].isUserData = true;
+        userAddresses.push(_userAddress);
+
+        // Log event
+        AddressAdded(_userAddress);
+    }
+
+    /// @dev Checks if a user address exists
+    /// @param _userAddress user address to be checked
+    function isUserData(address _userAddress) public constant returns(bool) {
+        return userDataStore[_userAddress].isUserData;
     }
 
 }
--- test/registry.js
+++ test/registry.js
@@ -1,8 +1,68 @@
 const Registry = artifacts.require("./Registry.sol");
 
 contract('Registry', function (accounts) {
-  it("placeholder test", async function () {
-    const instance = await  Registry.deployed();
-    assert.equal(1, 1);
+
+  describe('addUserAddress', function () {
+
+    it("anyone can add user addresses which can be retrieved", async function () {
+      const instance = await  Registry.deployed();
+
+      let userAddress_1 = "0x1000000000000000000000000000000000000000";
+      let userAddress_2 = "0x2000000000000000000000000000000000000000";
+
+      await instance.addUserAddress(userAddress_1, {from: accounts[0]});
+      await instance.addUserAddress(userAddress_2, {from: accounts[1]});
+
+      let savedAddress_1 = await instance.userAddresses(0);
+      assert.equal(savedAddress_1, userAddress_1);
+
+      let userData_1 = await instance.userDataStore(userAddress_1);
+      assert.equal(userData_1[0].toNumber(), 0, "State should be NEW");
+      assert.equal(userData_1[1], true);
+
+      let savedAddress_2 = await instance.userAddresses(1);
+      assert.equal(savedAddress_2, userAddress_2);
+
+      let userData_2 = await instance.userDataStore(userAddress_2);
+      assert.equal(userData_2[0].toNumber(), 0, "State should be NEW");
+      assert.equal(userData_2[1], true);
+    });
+
+    it("adding an already existing user address throws an error", async function () {
+      const instance = await  Registry.deployed();
+
+      let userAddress_1 = "0x1000000000000000000000000000000000000000";
+
+      try {
+        await instance.addUserAddress(userAddress_1, {from: accounts[0]});
+
+        // we shouldn't get to this point
+        assert(false, "Transaction should have failed");
+      }
+      catch (err) {
+        if (err.toString().indexOf("invalid opcode") < 0) {
+          assert(false, err.toString());
+        }
+      }
+    });
+
+
+    it("adding a null address throws an error", async function () {
+      const instance = await  Registry.deployed();
+
+      try {
+        await instance.addUserAddress(null, {from: accounts[0]});
+
+        // we shouldn't get to this point
+        assert(false, "Transaction should have failed");
+      }
+      catch (err) {
+        if (err.toString().indexOf("invalid opcode") < 0) {
+          assert(false, err.toString());
+        }
+      }
+    });
+
   });
+
 });
